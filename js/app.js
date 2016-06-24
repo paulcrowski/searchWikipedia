a3eec823348d481e518aca5c9050d3bc62bf2557
Pawel <p.p.wronski@gmail.com>
2016-06-24 22:09:52 +0200

    getjson callback on enter, keyup

--- js/app.js
+++ js/app.js
@@ -4,31 +4,50 @@
 console.log('test');
 
 $(document).ready(function() {
-    getQuote();
-    buttonRandom();
+    inputValue();
+//    buttonSearch();
 });
 
 
-function getQuote () {
-    // https://en.wikipedia.org/w/api.php?action=opensearch&search=dog&limit=10&namespace=0&format=json
-    var link = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
-    console.log(link);
-    $.getJSON(link, showQuote);
+function inputValue () {
+    // input check val on keyUp, check Enter
+    $("#search").keyup(function(e){
+       var searchValue = $(this).val();
+       $('#valueShow').html(searchValue);
+       getWiki(searchValue);
+       if(e.keyCode == 13)
+       {
+           alert('enter');
+       }
+    });
+
 }
 
-function showQuote(quoteData) {
+function getWiki(searchValue) {
+    // get wiki data
     console.log("---------------- data");
+    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchValue + "&limit=10&namespace=0&format=json&callback=?";
+    console.log(link);
+    // autocomplete
+    $.getJSON(link, showWiki);
 
 }
 
-function randomIntFromInterval(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
+function showWiki (dataWiki) {
+    var test =  dataWiki[1];
+    console.log(test);
+    //$('#wikiShow').html(test);
 }
 
-function buttonRandom () {
-    // button for choose random quote
+//function randomIntFromInterval(min,max) {
+//    return Math.floor(Math.random()*(max-min+1)+min);
+//}
+
+function buttonSearch () {
+    // button for click WIki Search
     $('.buttonRandom').click(function(){
         console.log('click');
+        var searchValue = $('#search').val();
 
     });
 }
