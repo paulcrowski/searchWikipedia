7969f2d1b721d3eed0fe9c11f0bd9b284137f5b1
Pawel <p.p.wronski@gmail.com>
2016-06-26 12:46:20 +0200

    wikie search AJAX ready with autcomplete

--- minjs/all.js
+++ minjs/all.js
@@ -4,50 +4,90 @@
 console.log('test');
 
 $(document).ready(function() {
+    // check input
     inputValue();
-//    buttonSearch();
+    // button ready for click
+    $('.buttonRandom').click(function(){
+        console.log('click');
+        var search = $('#search');
+        var searchValue = search.val();
+        var autocomplete = $('.autocomplete');
+        // search wiki and generate page
+        getWiki(searchValue);
+        // clear input and hide autcomplete
+        search.val("");
+        autocomplete.fadeOut();
+    });
 });
+
+
+
 
 
 function inputValue () {
     // input check val on keyUp, check Enter
     $("#search").keyup(function(e){
        var searchValue = $(this).val();
-       $('#valueShow').html(searchValue);
-       getWiki(searchValue);
+       // check length of input if less <0 hide autocomplete
+       if (searchValue.length > 0) {
+           $('.autocomplete').fadeIn();
+       } else {
+           $('.autocomplete').fadeOut();
+       }
+       // active autocomplete search, show autocomplete tips
+       autocompleteWiki(searchValue);
+       // if enter in input do click on button
        if(e.keyCode == 13)
        {
-           alert('enter');
+           $('.buttonRandom').click();
        }
+
     });
 
 }
 
-function getWiki(searchValue) {
-    // get wiki data
+// generator of search links
+function getLinkHelper (searchValue) {
     console.log("---------------- data");
-    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchValue + "&limit=10&namespace=0&format=json&callback=?";
+    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchValue + "&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&limit=10&namespace=0&format=json&callback=?";
     console.log(link);
-    // autocomplete
-    $.getJSON(link, showWiki);
-
+    return link;
 }
 
-function showWiki (dataWiki) {
-    var test =  dataWiki[1];
-    console.log(test);
-    //$('#wikiShow').html(test);
+// getjson for autocomplete
+function autocompleteWiki(searchValue) {
+    $.getJSON(getLinkHelper(searchValue), autocomplete);
+ }
+
+// getjson for wiki
+function getWiki(searchValue) {
+    $.getJSON(getLinkHelper(searchValue), showWiki);
 }
 
-//function randomIntFromInterval(min,max) {
-//    return Math.floor(Math.random()*(max-min+1)+min);
-//}
-
-function buttonSearch () {
-    // button for click WIki Search
-    $('.buttonRandom').click(function(){
-        console.log('click');
-        var searchValue = $('#search').val();
-
+//generete autocomplete after keyup in input
+function autocomplete (dataWiki)  {
+    var title =  dataWiki[1];
+    var autocomplete = $('.autocomplete');
+    var page = "";
+    $.each(title, function( index, value ) {
+        page = page + '<a class="wikiLink" href="'+dataWiki[3][index]+'">'+
+            value+ '</a><br/>';
     });
+    autocomplete.html(page);
 }
+
+// generete search after click or enter
+function showWiki (dataWiki) {
+    var title =  dataWiki[1];
+    var show = $('#wikiShow');
+    var page = "";
+    $.each(title, function( index, value ) {
+        page = page + '<a class="wikiLink" href="'+dataWiki[3][index]+'">'+
+                '<div class="panel panel-default">' +
+            '<div class="panel-heading">'+ value +'</div>'+
+            '<div class="panel-body">'+ dataWiki[2][index] + '</div>'
+            +'</div>' +'</a>';
+    });
+    show.html(page);
+}
+
